7585efd377bdf63dae4861ab3602a4fb2e3c2519
Ziyu Chen <zc839@nyu.edu>
2020-01-08 03:04:35 +0800

    Fixing heroku deployment

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 
-app.use(express.static(path.resolve(__dirname, "..", "dist")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.use(bodyParser.json());
 
