fcc9ddb00703c8c8eac7442e2b3dfd5c4eb2fe91
Ziyu Chen <zc839@nyu.edu>
2020-01-07 23:51:46 +0800

    Fixing heroku deployment

--- server/knexfile.js
+++ server/knexfile.js
@@ -13,7 +13,20 @@ module.exports = {
       directory: "../migrations",
     },
   },
-
+  staging: {
+    client: "pg",
+    connection:
+      process.env.DATABASE_URL ||
+      `postgres://${process.env.USER}@127.0.0.1:5432/eventpro`,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "../migrations",
+    },
+  },
   production: {
     client: "pg",
     connection:
